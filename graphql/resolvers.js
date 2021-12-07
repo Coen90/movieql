086ab0ac26e99066f74421c0cc2618961c3515b6
Hyuntae <bht9011@gmail.com>
2021-12-07 13:41:53 +0900

    2.0 movieql with graphQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,24 @@
-import { people, getById } from "./db";
+import {
+  people,
+  getByIdPerson,
+  getMovies,
+  getById,
+  addMovie,
+  deleteMovie,
+  getMoviesAPI,
+} from "./db";
 
 const resolvers = {
   Query: {
     people: () => people,
-    person: (_, { id }) => getById(id),
+    person: (_, { id }) => getByIdPerson(id),
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+    movieAPI: (_, { rating, limit }) => getMoviesAPI(limit, rating),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
